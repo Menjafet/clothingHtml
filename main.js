8dd4ee670fdf292d54e9267d6305381d575abd4c
Andres Villalobos <andres.villalobos@datagrove.com>
2023-05-23 12:02:27 -0600

    menu button shows and hides

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //hide menu when display is mobile
-const mobileMenu = () => {
-    if(document.getElementById('menu').display == 'flex'){
-        //nw
-        document.getElementById('menu').display = 'none';
-    }else{
-        document.getElementById('menu').display = 'flex';
-    }
-    //document.getElementById('menu-toggle').addEventListener('click', mobileMenu());
-}
-document.getElementById('menu-toggle').addEventListener('click', mobileMenu());
-const resetMenu = () => {
-    if(window.innerWidth >= 900){
-        document.getElementById('menu').display = 'flex';
-    }else if(window.innerWidth < 900){
-        document.getElementById('menu').display = 'none';
-    }
-}
-window.addEventListener('resize', resetMenu())
+// const mobileMenu = () => {
+//     if(document.getElementById('menu').display == 'flex'){
+//         //nw
+//         document.getElementById('menu').display = 'none';
+//     }else{
+//         document.getElementById('menu').display = 'flex';
+//     }
+//     //document.getElementById('menu-toggle').addEventListener('click', mobileMenu());
+// }
+// document.getElementById('menu-toggle').addEventListener('click', mobileMenu());
+// const resetMenu = () => {
+//     if(window.innerWidth >= 900){
+//         document.getElementById('menu').display = 'flex';
+//     }else if(window.innerWidth < 900){
+//         document.getElementById('menu').display = 'none';
+//     }
+// }
+// window.addEventListener('resize', resetMenu())
